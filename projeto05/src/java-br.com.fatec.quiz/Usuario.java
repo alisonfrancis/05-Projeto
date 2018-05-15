ff988b1b8a3b1322b3e5d2cd883ff8552975eda9
Baden Damasio <36901235+Baden286@users.noreply.github.com>
2018-05-15 01:29:37 -0300

    Usuario.java

---
+++ projeto05/src/java-br.com.fatec.quiz/Usuario.java
@@ -0,0 +1,26 @@
+
+package br.com.fatec.quiz;
+
+
+public class Usuario {
+    
+    private String UserName;
+            private Double ponto;
+
+    public Double getPonto() {
+        return ponto;
+    }
+
+    public void setPonto(Double ponto) {
+        this.ponto = ponto;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+    
+}
